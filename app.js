ad6eef55d83a14d4ada95d10c48cbe4770f6d78c
Clayton Hendricks <ClaytonGHendricks@Gmail.Com>
2017-11-07 20:43:47 -0600

    Adding bootstrap theme, default... starting customization

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require('express');
 
-var hi = 'hello';
+var app = express();
 
-if (true) {
-    console.log('hi');
-}
-console.log(hi);
+var port = 5555;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+app.get('/books', function (req, res) {
+    res.send('hello BOOKS');
+});
+
+app.listen(port, function (err) {
+    console.log('running on port ' + port);
+});
